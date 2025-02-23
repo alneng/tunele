170b6ff501c032ea4a3b42d017d62b1d9f3777c1
Alan <87085831+alneng@users.noreply.github.com>
2025-02-22 22:16:44 -0500

    Fix tests

--- src/backend/src/config/index.ts
+++ src/backend/src/config/index.ts
@@ -1,17 +1,19 @@
 export const NODE_ENV = process.env.NODE_ENV || "development";
 
 // Throw if any required environment variables are missing
-if (!process.env.CORS_OPTIONS)
-  throw new Error("Missing environment variable: CORS_OPTIONS");
-if (!process.env.COOKIE_SETTINGS)
-  throw new Error("Missing environment variable: COOKIE_SETTINGS");
-if (!process.env.SPOTIFY_CLIENT_KEY)
-  throw new Error("Missing environment variable: SPOTIFY_CLIENT_KEY");
+if (NODE_ENV !== "test") {
+  if (!process.env.CORS_OPTIONS)
+    throw new Error("Missing environment variable: CORS_OPTIONS");
+  if (!process.env.COOKIE_SETTINGS)
+    throw new Error("Missing environment variable: COOKIE_SETTINGS");
+  if (!process.env.SPOTIFY_CLIENT_KEY)
+    throw new Error("Missing environment variable: SPOTIFY_CLIENT_KEY");
+}
 
 export const PORT = process.env.PORT ? Number(process.env.PORT) : 7600;
 
-export const CORS_OPTIONS = JSON.parse(process.env.CORS_OPTIONS);
-export const COOKIE_SETTINGS = JSON.parse(process.env.COOKIE_SETTINGS);
+export const CORS_OPTIONS = JSON.parse(process.env.CORS_OPTIONS || "{}");
+export const COOKIE_SETTINGS = JSON.parse(process.env.COOKIE_SETTINGS || "{}");
 
 export const SPOTIFY_CLIENT_KEY = process.env.SPOTIFY_CLIENT_KEY;
 
