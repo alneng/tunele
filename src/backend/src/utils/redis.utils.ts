40cf95c572461623111c2d6c22d3ff47a3051f5c
Alan <87085831+alneng@users.noreply.github.com>
2026-02-14 17:08:57 -0500

    Cache main game daily tracks

--- src/backend/src/utils/redis.utils.ts
+++ src/backend/src/utils/redis.utils.ts
@@ -1,7 +1,10 @@
 export const CacheKeys = {
   SPOTIFY_ACCESS_TOKEN: "cache:spotify_access_token",
+
+  // Main Game
+  MAIN_GAME_TRACK: (date: string) => `cache:main:game_track:${date}`,
+
+  // Custom Game
   PLAYLIST_GAME_TRACK: (playlistId: string, date: string) =>
     `cache:spotify_playlist:${playlistId}:game_track:${date}`,
-  PLAYLIST_TRACKS: (playlistId: string, snapshotId: string) =>
-    `cache:spotify_playlist:${playlistId}:tracks:${snapshotId}`,
 };
